de4ce0f58a579392b5562c634d88f594d17e1013
Yongbok Kim (ruo91) <ruo91@yongbok.net>
2018-01-22 05:07:26 -0500

    ok

---
+++ push-to-dockerio.sh
@@ -0,0 +1,4 @@
+docker tag rockplace/tomcat7-s2i-openshift:7.0.82 docker.io/rockplace/tomcat7-s2i-openshift:7.0.82
+docker tag rockplace/tomcat7-s2i-openshift:7.0.82 docker.io/rockplace/tomcat7-s2i-openshift:latest
+docker push docker.io/rockplace/tomcat7-s2i-openshift:7.0.82
+docker push docker.io/rockplace/tomcat7-s2i-openshift:latest
